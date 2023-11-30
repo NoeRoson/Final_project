f1fcc70a5f54aef3e1166ef3fd0ae655c9abc492
Noelia Roson <noelia.roson@gmail.com>
2023-11-30 10:51:19 +0100

    limpieza llamadas

--- src/suport.py
+++ src/suport.py
@@ -6,17 +6,24 @@ warnings.filterwarnings('ignore')
 import re
 
 
-def lower_tildes(columna):
+def lower_tildes(column):
     '''Entra una columna:
     Se convierte a minúsculas y se eliminan tildes
     '''
-    return columna.apply(lambda x: unidecode(str(x).lower()))
+    return column.apply(lambda x: unidecode(str(x).lower()))
 
-def format_col(columns):
-    '''Entra un df.columns:
+def format_filas(column):
+    '''Entra un df.column:
     Se convierte a minúsculas y se eliminan tildes
     '''
-    return columns.map(lambda x: unidecode(x.lower()))
+    return column.map(lambda x: unidecode(x.lower()))
+
+def unif_col(columns):
+    '''Entran todas las columnas de un df:
+    Se convierte a minúsculas y se eliminan tildes
+    '''
+    return map(lambda x: unidecode(x.lower()), columns)
+
 
 
 def clean_year(columna):
@@ -24,7 +31,7 @@ def clean_year(columna):
     Entra una columna:
     Elimina caracteres no numéricos y convierte a tipo de dato de año
     '''
-    columna = columna.astype(str)  # Asegúrate de que la columna sea de tipo cadena
+    columna = columna.astype(str) #  Nos aseguramos de que la columna sea de tipo cadena
     columna = columna.str.replace(r'\D', '', regex=True)  # Elimina caracteres no numéricos
     columna = pd.to_datetime(columna, format='%Y', errors='coerce')  # Convierte a tipo de dato de año
     return columna.dt.year  # Extrae el año de la fecha resultante
